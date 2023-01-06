6599b6a69009c9037c5d30b2161a50001c9538c2
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-06 11:49:38 +0530

    Login page and a landing page.

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,13 @@
 <template>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-      <div class="text-center lg:text-left">
-        <h1 class="text-5xl font-bold">Login now!</h1>
-        <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
-      </div>
-      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <div class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="text"
-              placeholder="email"
-              class="input input-bordered"
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="text"
-              placeholder="password"
-              class="input input-bordered"
-            />
-            <label class="label">
-              <a href="#" class="label-text-alt link link-hover"
-                >Forgot password?</a
-              >
-            </label>
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary">Login</button>
-          </div>
-        </div>
-      </div>
+    <div class="hero min-h-screen" style="background-image: url(https://placeimg.com/1000/800/nature);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-center text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Veggies</h1>
+      <p class="mb-5">Service delivering salads and consumables directly to your home/work. Choose a healthy lifestyle.</p>
+      <router-link to="login"></router-link>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
+</div>
 </template>
---
+++ src/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hero min-h-screen" style="background: rgb(157,177,173); background: linear-gradient(31deg, rgba(157,177,173,1) 0%, rgba(0,148,28,1) 35%, rgba(255,229,0,1) 100%);">
+    <div class="hero-overlay bg-opacity-30"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <!-- <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div> -->
+      <div class="card w-96 glass">
+
+        <div class="card-body">
+          <h2 class="card-title">Login</h2>
+          <p>Please login to continue.</p>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Email</span>
+              <span class="label-text-alt"></span>
+            </label>
+
+            <input type="text" placeholder="example@domain.com"
+              class="input input-bordered w-full max-w-xs text-black" />
+          </div>
+            <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Password</span>
+              <span class="label-text-alt"></span>
+            </label>
+
+            <input type="password" placeholder="*******"
+              class="input input-bordered w-full max-w-xs text-black" />
+          </div>
+          <div class="card-actions justify-center">
+            <button class="btn btn-primary">Login!</button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
